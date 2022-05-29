e9b158453dce03fb13371c4d1958e586f9fecb0e
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-28 19:31:38 -0600

    Updated Schema to initial layout

--- models/character.js
+++ models/character.js
@@ -2,7 +2,41 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const characterSchema = new Schema({
-    name:String
+    name: String,
+    race: String,
+    class: String, 
+    armorClass: Number,
+    health: Number,
+    abilityScores: {
+        strength: Number,
+        dexterity: Number,
+        constitution: Number,
+        wisdom: Number,
+        intelligence: Number,
+        charisma: Number,
+    },
+    background: String,
+    proficiencies: {
+        athletics: Boolean,
+        acrobatics: Boolean,
+        sleightOfHand: Boolean,
+        stealth: Boolean,
+        arcana: Boolean,
+        history: Boolean,
+        investigation: Boolean,
+        nature: Boolean,
+        religion: Boolean,
+        animalHandling: Boolean,
+        insight: Boolean,
+        medicine: Boolean,
+        perception: Boolean,
+        survival: Boolean,
+        deception: Boolean,
+        intimidation: Boolean,
+        performance: Boolean,
+        persuasion: Boolean,
+    },
+
 
 })
 
