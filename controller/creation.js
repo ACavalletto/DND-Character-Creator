32e23d0396f979f4b6c0687f8a7a512dec4f398d
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-29 15:24:43 -0600

    added basic pathing from race to class page and then to background page as well as

--- controller/creation.js
+++ controller/creation.js
@@ -6,4 +6,22 @@ router.get('/race', (req, res) => {
     res.render('creation/race.ejs')
 })
 
+//Create Routes
+router.post('/race', (req, res) => {
+    Character.create(req.params.id, (err, createdCharacter) => {
+        res.redirect(`/creation/${createdCharacter._id}/class`)
+    })
+})
+router.post('/:id/class', (req, res) => {
+    Character.findByIdAndUpdate(req.params.id, req.body, () => {
+        res.redirect(`/creation/:id/background`)
+    })
+})
+
+router.get('/:id/class', (req, res) => {
+    res.render("creation/class.ejs")
+})
+router.get('/:id/background', (req, res) => {
+    res.send("background page")
+})
 module.exports = router
