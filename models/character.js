b3a9a6986d5ff22cbb72cc376e10d71106923525
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-31 19:52:22 -0600

    added more content to elf page and added more input values to pass on to character schema.

--- models/character.js
+++ models/character.js
@@ -1,52 +1,33 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const characterSchema = new Schema({
-    name: String,
-    race: String,
-    class: String,
-    armorClass: Number,
-    health: Number,
-    age: Number,
-    speed: Number,
-    size: String,
-    darkVision: String,
-    resistance: [{ type: String }],
-    savingThrow: String,
-    languages: [{ type: String }],
-    stats: {
-        strength: String,
-        dexterity: String,
-        constitution: String,
-        intelligence: String,
-        wisdom: String,
-        charisma: String,
-    },
-    proficiencies: {
-        athletics: Boolean,
-        acrobatics: Boolean,
-        sleightOfHand: Boolean,
-        stealth: Boolean,
-        arcana: Boolean,
-        history: Boolean,
-        investigation: Boolean,
-        nature: Boolean,
-        religion: Boolean,
-        animalHandling: Boolean,
-        insight: Boolean,
-        medicine: Boolean,
-        perception: Boolean,
-        survival: Boolean,
-        deception: Boolean,
-        intimidation: Boolean,
-        performance: Boolean,
-        persuasion: Boolean,
-    },
-    cantrips: [{ type: String }],
-})
+  name: String,
+  race: String,
+  class: String,
+  armorClass: Number,
+  health: Number,
+  age: Number,
+  speed: Number,
+  size: String,
+  darkVision: String,
+  resistance: [{ type: String }],
+  savingThrow: String,
+  languages: [{ type: String }],
+  stats: {
+    strength: String,
+    dexterity: String,
+    constitution: String,
+    intelligence: String,
+    wisdom: String,
+    charisma: String,
+  },
+  proficiencies: [{ type: String }],
+  cantrips: [{ type: String }],
+  racialFeature: [{ type: String }],
+  classFeature: [{ type: String }],
+});
 
+const Character = mongoose.model("Character", characterSchema);
 
-
-const Character = mongoose.model("Character", characterSchema)
-
-module.exports = Character
+module.exports = Character;
